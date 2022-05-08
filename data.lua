0723599788479d9996f5e4a3f54e343b2b052526
RustyNova <rusty.nova.jsb@gmail.com>
2022-05-08 17:41:06 +0200

    first commit

---
+++ data.lua
@@ -0,0 +1,11 @@
+local explosion_sounds = 
+{
+    {filename = "__Ekusplosion__/sounds/explosion2.ogg",},
+    {filename = "__Ekusplosion__/sounds/plosion.ogg",},
+    {filename = "__Ekusplosion__/sounds/losion.ogg",},
+    {filename = "__Ekusplosion__/sounds/sion.ogg",}
+}
+
+data.raw.gun["rocket-launcher"].attack_parameters.sound = explosion_sounds
+data.raw.gun["tank-cannon"].attack_parameters.sound = explosion_sounds
+data.raw.gun["artillery-wagon-cannon"].attack_parameters.sound = explosion_sounds
